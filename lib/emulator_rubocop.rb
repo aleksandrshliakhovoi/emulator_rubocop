943ee501d2941c19c15142da78ad6317ec580eee
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2022-08-02 13:14:45 +0300

    Add logic

--- lib/emulator_rubocop.rb
+++ lib/emulator_rubocop.rb
@@ -1,8 +1,59 @@
 # frozen_string_literal: true
 
 require_relative "modules/version"
+require_relative "modules/errors"
 
 class EmulatorRubocop
-  class Error < StandardError; end
-  # Your code goes here...
+  include Modules
+
+  def initialize(args)
+    @args = args
+    @arr_of_files_path = check_file_path?(args[0]) || Dir['./app/*.rb'] + Dir['./web/*.rb']
+    @founded_errors = {}
+    @specific_cop = args[1]
+  end
+
+  def call
+    find_warnings
+    show_errors
+  end
+
+  private
+
+  attr_reader :arr_of_files_path, :founded_errors, :specific_cop, :args
+
+  def find_warnings
+    arr_of_files_path.each do |file_path|
+      file = File.open(file_path, 'r')
+      file_data_array = file.readlines
+
+      file_data_array.each_with_index do |el, idx|
+        case
+        when el.include?('binding.pry')
+          founded_errors[:binding_pry] = message(file_path, idx, Modules::Errors::BindingError.new)
+        when el.include?('.send')
+          founded_errors[:send] = message(file_path, idx, Modules::Errors::SendMethodError.new)
+        when el.include?('.public_send')
+          founded_errors[:public_send] = message(file_path, idx, Modules::Errors::SendMethodError.new)
+        end
+      end
+      file.close
+    end
+  end
+
+  def check_file_path?(file_path)
+    file_path ? [file_path] : nil
+  end
+
+  def message(file_path, index, error)
+    "#{file_path}:#{index+1}: #{error}:"
+  end
+
+  def show_errors
+    if specific_cop
+      puts "#{founded_errors[specific_cop.to_sym]} found #{specific_cop}"
+    else
+      founded_errors.each { |key, value| puts "#{value} found #{key}" }
+    end
+  end
 end
